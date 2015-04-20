bc1ea9e1a5330391a6978cb4f47a35e9ad397c87
gerrygooner <gerrygooner@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-20 21:38:12 +0000

    Fixed Plugin URI.
    git-svn-id: https://plugins.svn.wordpress.org/wp-cleandb/tags/1.2.1@1139801 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-cleandb.php
+++ wp-cleandb.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: WP-CleanDB
-Plugin URI: http://gerrytucker.co.uk/wp-plugins/wpclean-db.zip
+Plugin URI: https://wordpress.org/plugins/wp-cleandb.1.2.1.zip
 Description: Cleanup your Wordpress database in one click!
-Version: 1.0
+Version: 1.2.1
 Author: Gerry Tucker
 Author URI: http://gerrytucker.co.uk/
 License: GPLv2 or later
