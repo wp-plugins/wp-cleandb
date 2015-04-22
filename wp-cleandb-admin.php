6f393f3cef339e5137f8b79987fbc7ba925d450b
gerrygooner <gerrygooner@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-22 13:56:40 +0000

    Bit more tidying up.
    git-svn-id: https://plugins.svn.wordpress.org/wp-cleandb/trunk@1142196 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-cleandb-admin.php
+++ wp-cleandb-admin.php
@@ -406,7 +406,8 @@ function Division($total, $division) {
 			
 		</table>
 		
-		<p>Make sure you have a backup of you WordPress database before cleanup!<br />
+		<p>Make sure you have a backup of your WordPress database before cleanup!</p>
+		<p>
 			<input type="submit" name="submit" value="Cleanup the selected items!" style="width: 300px;" class="button-primary">
 		</p>
 	</form>
